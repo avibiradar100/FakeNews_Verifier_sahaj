833affab9dd41db24c1e00aa8a7783821a209e5c
Avinash Biradar <biradarav.100@gmail.com>
2022-05-12 01:57:27 +0530

    Added files

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -108,3 +108,132 @@ exports.downvotePost = async (req, res) => {
     });
   }
 };
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      res.status(404).json({
+        success: false,
+        message: "post not found",
+      });
+    }
+
+    if (post.owner.toString() !== req.user._id.toString()) {
+      res.status(401).json({
+        success: false,
+        message: "Unauthorized",
+      });
+    }
+    await post.remove();
+
+    const user = await User.findById(req.user._id);
+
+    const index = user.posts.indexOf(req.params.id);
+
+    user.posts.splice(index, 1);
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Post deleted ",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.commentOnPost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    let commentIndex = -1;
+
+    //checking if comment already exists
+    post.comments.forEach((item, index) => {
+      if (item.user.toString() === req.user._id.toString()) {
+        commentIndex = index;
+      }
+    });
+
+    if (commentIndex !== -1) {
+      post.comments[commentIndex].comment = req.body.comment;
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "Comment updated",
+      });
+    } else {
+      post.comments.push({
+        user: req.user._id,
+        comment: req.body.comment,
+      });
+
+      await post.save();
+
+      res.status(200).json({
+        success: true,
+        message: "Comment added",
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+exports.deleteComment = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    //chcking if owner wants to delete
+    if (post.owner.toString() === req.user._id.toString()) {
+      if (req.body.commentId === undefined) {
+        return res.status(404).json({
+          success: false,
+          message: "Comment Id is required",
+        });
+      }
+
+      post.comments.forEach((item, index) => {
+        if (item._id.toString() === req.body.commentId.toString()) {
+          return post.comments.splice(index, 1);
+        }
+      });
+
+      await post.save();
+
+      return res.status(200).json({
+        success: true,
+        message: "selected comment has deleted",
+      });
+    } 
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createPost, upvotePost,downvotePost } = require("../controllers/post");
+const { createPost, upvotePost,downvotePost, deletePost } = require("../controllers/post");
 const { isAuthenticated } = require("../middlewares/auth");
 
 const router = express.Router();
@@ -7,5 +7,11 @@ const router = express.Router();
 router.route("/post/upload").post(isAuthenticated, createPost);
 router.route("/post/upvote/:id").get(isAuthenticated, upvotePost);
 router.route("/post/downvote/:id").get(isAuthenticated,downvotePost)
+router.route("/posts").get(isAuthenticated, getPostOfFollowing);
+router.route("/post/:id").delete(isAuthenticated, deletePost);
+router
+  .route("/post/comment/:id")
+  .put(isAuthenticated, commentOnPost)
+  .delete(isAuthenticated, deleteComment);
 
 module.exports = router;
